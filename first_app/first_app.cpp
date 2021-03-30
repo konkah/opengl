ab5828db402b413caccb2e1a7cbaf8602997f9b4
Karlos Helton Braga <karlos.hbraga@gmail.com>
2021-03-30 20:15:16 -0300

    example app

---
+++ first_app/first_app.cpp
@@ -0,0 +1,50 @@
+// first_app.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include<GL/glut.h>
+
+void display() {
+	glClear(GL_COLOR_BUFFER_BIT);
+	glColor3f(1.0, 0.0, 0.0);
+
+	glBegin(GL_POINTS);
+	glVertex2f(10.0, 10.0);
+	glVertex2f(150.0, 80.0);
+	glVertex2f(100.0, 20.0);
+	glVertex2f(200.0, 100.0);
+	glEnd();
+	glFlush();
+}
+
+void myinit() {
+	glClearColor(1.0, 1.0, 1.0, 1.0);
+	glColor3f(1.0, 0.0, 0.0);
+	glPointSize(5.0);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluOrtho2D(0.0, 499.0, 0.0, 499.0);
+}
+
+void main(int argc, char** argv) {
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+	glutInitWindowSize(500, 500);
+	glutInitWindowPosition(0, 0);
+	glutCreateWindow("Points");
+	glutDisplayFunc(display);
+
+	myinit();
+	glutMainLoop();
+}
+
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
